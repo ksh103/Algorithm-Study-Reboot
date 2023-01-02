e32af997d2eff4c0128b29387f6483d214108f5b
DONG-8 <junzzamg9@gmail.com>
2023-01-02 21:37:42 +0900

    add : 230102 boj_1912_연속합 Solved

---
+++ 230102/박동준_boj_1912_연속합.py
@@ -0,0 +1,15 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+dp = [0]*len(arr)
+
+dp[0] = arr[0]
+max_num = dp[0]
+
+for i in range(1,len(arr)):
+    dp[i] = max(dp[i-1] + arr[i], arr[i], arr[i] + arr[i-1])
+
+    if max_num < dp[i]:
+        max_num = dp[i]
+
+print(max_num)
